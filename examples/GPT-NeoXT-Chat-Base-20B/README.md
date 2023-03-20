1532e8dc8280d1dbee66f14191a9afaf5bfa2e58
lno <contact@louishenault.com>
2023-03-20 19:24:25 +0000

    Add example for OpenChat model

---
+++ examples/GPT-NeoXT-Chat-Base-20B/README.md
@@ -0,0 +1,22 @@
+# OpenChatKit service
+
+## Description
+
+This project use the [OpenChatKit](https://github.com/togethercomputer/OpenChatKit) model and turns it into an gRPC service that can be used through [SimpleAI](https://github.com/lhenault/simpleAI).
+
+To quote the project:
+
+>
+    OpenChatKit provides a powerful, open-source base to create both specialized and general purpose chatbots for various applications. The kit includes an instruction-tuned 20 billion parameter language model, a 6 billion parameter moderation model, and an extensible retrieval system for including up-to-date responses from custom repositories. It was trained on the OIG-43M training dataset, which was a collaboration between Together, LAION, and Ontocord.ai. Much more than a model release, this is the beginning of an open source project. We are releasing a set of tools and processes for ongoing improvement with community contributions.
+
+## Build
+
+```bash
+docker build . -t openchatkit-service:latest
+```
+
+## Start service
+
+```bash
+docker run -it --rm --p 50051:50051 --gpus all openchatkit-service:latest
+```
---
+++ examples/GPT-NeoXT-Chat-Base-20B/model.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from simple_ai.serve.python.server import LanguageModel
+
+
+from get_models import MODEL_ID
+
+@dataclass(unsafe_hash=True)
+class OpenChatModel(LanguageModel):
+    gpu_id:     int=0
+    device      = torch.device('cuda', gpu_id)
+    model       = AutoModelForCausalLM.from_pretrained(MODEL_ID).half()
+    tokenizer   = AutoTokenizer.from_pretrained(MODEL_ID)
+
+    def __post_init__(self):
+        self.model.to(self.device)
+        
+    def complete(self, 
+        prompt:             str='<|endoftext|>',
+        max_tokens:         int=512, 
+        temperature:        float=0.6,
+        *args,
+        **kwargs
+    ) -> str:
+        
+        inputs = (
+            self.tokenizer(prompt, return_tensors='pt')
+            .to(self.model.device)
+        )
+        outputs = self.model.generate(
+            **inputs,
+            max_new_tokens=max_tokens,
+            do_sample=True,
+            temperature=temperature,
+            top_k=40,
+            pad_token_id=self.tokenizer.eos_token_id
+        )
+        output = self.tokenizer.batch_decode(outputs)[0]
+
+        # remove the context from the output
+        output = output[len(prompt):]
+
+        return output
